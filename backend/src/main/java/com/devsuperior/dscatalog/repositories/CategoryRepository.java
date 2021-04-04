7c04592baa98f9b6b59033492e27605bccdab958
Jéssica Góis <jessica.gois.santos@gmail.com>
2021-04-03 21:10:14 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.Category;
 
+//Camada de Acesso a Dados
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
